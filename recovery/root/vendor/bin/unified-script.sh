382719bd6ff6dbf0189fa96a30163d0a394cdf64
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-05 01:15:55 +0800

    E6746 + E7746: Re aline

--- recovery/root/vendor/bin/unified-script.sh
+++ recovery/root/vendor/bin/unified-script.sh
@@ -12,10 +12,10 @@ load_E6746()
 load_E7746()
 {
     resetprop "ro.product.model" "IN_Note1"
-	resetprop "ro.product.name" "E7746"
-	restprop "ro.product.system.model" "IN_Note1"
-	resetprop "ro.product.device" "E7746"
-	resetprop "ro.product.vendor.device" "E7746"
+    resetprop "ro.product.name" "E7746"
+    restprop "ro.product.system.model" "IN_Note1"
+    resetprop "ro.product.device" "E7746"
+    resetprop "ro.product.vendor.device" "E7746"
 }
 	
 unknown=$(getprop ro.mmx.sku.name)
