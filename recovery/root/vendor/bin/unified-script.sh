d6302caae444049a91648cf06c50e1e2c22c9bd6
mastersenpai05 <pampangavergil0405@gmail.com>
2021-08-05 01:31:55 +0800

    E6746 + E7746: Add target device &  add missing

--- recovery/root/vendor/bin/unified-script.sh
+++ recovery/root/vendor/bin/unified-script.sh
@@ -13,7 +13,7 @@ load_E7746()
 {
     resetprop "ro.product.model" "IN_Note1"
     resetprop "ro.product.name" "E7746"
-    restprop "ro.product.system.model" "IN_Note1"
+    resetprop "ro.product.system.model" "IN_Note1"
     resetprop "ro.product.device" "E7746"
     resetprop "ro.product.vendor.device" "E7746"
 }
