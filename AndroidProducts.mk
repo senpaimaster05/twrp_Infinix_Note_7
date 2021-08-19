4a03bc2b979d2d8539853359bed9b15b46201a7a
mastersenpai05 <pampangavergil0405@gmail.com>
2021-08-20 00:09:40 +0800

    E7746: Initial Bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,7 +17,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/omni_E6746.mk
+     $(LOCAL_DIR)/omni_E7746.mk
 
 COMMON_LUNCH_CHOICES := \
- omni_E6746-eng
+     omni_E7746-eng
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/micromax/E6746
+DEVICE_PATH := device/micromax/E7746
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
